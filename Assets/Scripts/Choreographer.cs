e0aea345fc884dfb1109d37934a8eeadaf1dd40f
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2016-02-05 13:02:09 +1030

    Add timings management

---
+++ Assets/Scripts/Choreographer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Choreographer : MonoBehaviour {
+
+    private Timings timings;
+    private AudioSource music;
+    private AudioSource positiveSoundEffect;
+    private AudioSource negativeSoundEffect;
+
+    private bool userMadeAnAttempt = false;
+
+    Choreographer(Timings timings) {
+        this.timings = timings;
+    }
+
+	// Use this for initialization
+	private void Start () {
+        Debug.Log("Started even when set to private");
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+	       
+	}
+
+    private void FixedUpdate()
+    {
+        if (userMadeAnAttempt)
+        {
+            Timings.TimingResult result = timings.checkAttempt(music.time);
+            Debug.Log("Result was " + result.ToString());
+            if (result == Timings.TimingResult.GOOD || 
+                result == Timings.TimingResult.BAD)
+            {
+                positiveSoundEffect.Play();
+            }
+            userMadeAnAttempt = false;
+        }
+        else
+        {
+            if (timings.checkForMiss(music.time) == Timings.TimingResult.MISS)
+            {
+                negativeSoundEffect.Play();
+            }
+        }
+    }
+
+    public void UserBeat()
+    {
+        userMadeAnAttempt = true;
+    }
+}
---
+++ Assets/Scripts/Timings.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Timings 
+{
+    private const float leeway = 500;
+    private const float leewayMiss = 250;
+
+    private int currentTimingIndex = -1;
+    private float currentTiming;
+
+    private List<float> timings;
+
+    public enum TimingResult
+    {
+        GOOD, BAD, MISS, IGNORE_ATTEMPT
+    }
+
+    Timings(List<float> timings)
+    {
+        this.timings = timings;
+        moveToNextTiming();
+    }
+
+    private void moveToNextTiming()
+    {
+        currentTimingIndex++;
+        currentTiming = timings[currentTimingIndex];
+    }
+
+    public TimingResult checkForMiss(float ellapsedTime)
+    {
+        if (ellapsedTime > currentTiming + leeway + leewayMiss)
+        {
+            moveToNextTiming();
+            return TimingResult.MISS;
+        }
+        return TimingResult.IGNORE_ATTEMPT;
+    }
+
+    public TimingResult checkAttempt(float ellapsedTime)
+    {
+        if (ellapsedTime > currentTiming - leeway && ellapsedTime < currentTiming + leeway)
+        {
+            moveToNextTiming();
+            return TimingResult.GOOD;
+        }
+        else if (ellapsedTime > currentTiming - leeway - leewayMiss && ellapsedTime < currentTiming + leeway + leewayMiss)
+        {
+            moveToNextTiming();
+            return TimingResult.BAD;
+        }
+        else
+        {
+            return TimingResult.IGNORE_ATTEMPT;
+        }
+    }
+}
