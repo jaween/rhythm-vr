e7cdc330a0e9ddc8bf0f1bbe4cd1c255437fb0de
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2016-02-14 12:57:51 +1030

    Add end scene

---
+++ Assets/EndController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndController : MonoBehaviour
+{
+    public Text resultsText;
+    public Text congratulationsText;
+    public Text nameText;
+    public ScreenFader fader;
+
+	private void Start ()
+    {
+        int goodTimings = PlayerPrefs.GetInt("good", 0);
+        int badTimings = PlayerPrefs.GetInt("bad", 0);
+        int missTimings = PlayerPrefs.GetInt("miss", 0);
+
+        // Sets the text
+        string results =
+            "Good: " + goodTimings + "\n" +
+            "Bad: " + badTimings + "\n" +
+            "Miss: " + missTimings;
+        resultsText.text = results;
+        congratulationsText.text = "Thanks for playing!";
+        nameText.text = "Made by Jaween Ediriweera\nFebruary 2016";
+
+        bool fadeFromBlack = true;
+        float duration = 2.5f;
+        fader.Fade(fadeFromBlack, duration);
+    }
+}
